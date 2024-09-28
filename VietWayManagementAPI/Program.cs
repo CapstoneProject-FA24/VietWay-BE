aae4687c3e1ba6d4912fa9a1dccdc0961d886d01
NeonBlaze101 <dotoan22112003@gmail.com>
2024-09-28 20:46:31 +0700

    Config api

--- VietWayManagementAPI/Program.cs
+++ VietWayManagementAPI/Program.cs
@@ -1,4 +1,10 @@
 
+using IdGen.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using VietWayManagementAPI.Mappers;
+
 namespace VietWayManagementAPI
 {
     public class Program
@@ -13,7 +19,43 @@ namespace VietWayManagementAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            builder.Services.AddIdGen(0);
+            builder.Services.AddAutoMapper(typeof(MappingProfile));
+            
+            builder.Services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                string issuer;
+                string audience;
+                string secretKey;
+                if (builder.Environment.IsDevelopment())
+                {
+                    issuer = builder.Configuration["Jwt:Issuer"];
+                    audience = builder.Configuration["Jwt:Audience"];
+                    secretKey = builder.Configuration["Jwt:SecretKey"];
+                } else
+                {
+                    issuer = Environment.GetEnvironmentVariable("Jwt:Issuer");
+                    audience = Environment.GetEnvironmentVariable("Jwt:Audience");
+                    secretKey = Environment.GetEnvironmentVariable("Jwt:SecretKey");
+                }
+                o.UseSecurityTokenValidators = true;
+                o.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidateAudience = true,
+                    ValidAudience = audience,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
