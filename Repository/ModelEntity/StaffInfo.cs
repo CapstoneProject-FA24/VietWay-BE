584c27d6f53ce5db004481a488aee4ac038d24cd
NeonBlaze101 <dotoan22112003@gmail.com>
2024-09-28 20:45:55 +0700

    Update Repository

--- Repository/ModelEntity/StaffInfo.cs
+++ Repository/ModelEntity/StaffInfo.cs
@@ -3,17 +3,14 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Repository.ModelEntity
 {
-    public class CompanyStaffInfo
+    public class StaffInfo
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int StaffId { get; set; }
         [ForeignKey(nameof(Account))]
         public int AccountId { get; set; }
         public required string FullName { get; set; }
-        [ForeignKey(nameof(Company))]
-        public int CompanyId { get; set; }
 
         public virtual Account? Account { get; set; }
-        public virtual Company? Company { get; set; }
     }
 }
