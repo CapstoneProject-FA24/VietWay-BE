4dab4b9be2a065089207fdeb565af21801392511
NeonBlaze101 <dotoan22112003@gmail.com>
2024-10-05 10:17:43 +0700

    test vnpay

--- VietWay.API.Customer/Program.cs
+++ VietWay.API.Customer/Program.cs
@@ -1,4 +1,12 @@
 
+using IdGen;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using VietWay.Service.ThirdParty;
+using VietWay.Util.IdHelper;
+
 namespace VietWay.API.Customer
 {
     public class Program
@@ -7,29 +15,123 @@ namespace VietWay.API.Customer
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            #region builder.Services.AddAuthentication(...);
+            builder.Services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                string? issuer = builder.Configuration["Jwt:Issuer"]
+                        ?? throw new Exception("Can not get JWT Issuer");
+                string? audience = builder.Configuration["Jwt:Audience"]
+                    ?? throw new Exception("Can not get JWT Audience");
+                string secretKey;
+                if (builder.Environment.IsDevelopment())
+                {
+                    secretKey = builder.Configuration["Jwt:Key"]
+                        ?? throw new Exception("Can not get JWT Key");
+                }
+                else
+                {
+                    secretKey = Environment.GetEnvironmentVariable("PROD_JWT_KEY")
+                        ?? throw new Exception("Can not get JWT Key");
+                }
+                o.UseSecurityTokenValidators = true;
+                o.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidateAudience = true,
+                    ValidAudience = audience,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey)),
+                    ValidateLifetime = true
+                };
+            });
+            #endregion
+            #region builder.Services.AddCors(...);
+            builder.Services.AddCors(option =>
+            {
+                option.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+            #endregion
+            #region builder.Services.AddSwaggerGen(...);
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                new OpenApiInfo
+                { Title = "VietWay API", Description = "API for VietWay", Version = "1.0.0" });
+                options.AddSecurityDefinition("Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        In = ParameterLocation.Header,
+                        Description = "Please enter token",
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.Http,
+                        BearerFormat = "JWT",
+                        Scheme = "bearer"
+                    });
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
+            #endregion
+            #region builder.Services.AddScoped(...);
+            builder.Services.AddScoped<IVnPayService, VnPayService>();
 
+            #endregion
+            builder.Services.AddSingleton<IIdGenerator, SnowflakeIdGenerator>();
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            app.UseStaticFiles();
+            #region app.UseSwagger(...);
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwaggerUI(c =>
+                {
+                    c.InjectStylesheet("/SwaggerDark.css");
+                    c.EnableTryItOutByDefault();
+                });
             }
-
+            else
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "VietWay Customer Api");
+                    c.InjectStylesheet("/SwaggerDark.css");
+                    c.RoutePrefix = "";
+                    c.EnableTryItOutByDefault();
+                });
+            }
+            #endregion
             app.UseHttpsRedirection();
-
+            app.UseCors("AllowAll");
+            app.UseAuthentication();
             app.UseAuthorization();
-
-
+            //app.UseMiddleware<ErrorHandlingMiddleware>();
             app.MapControllers();
-
             app.Run();
         }
     }
