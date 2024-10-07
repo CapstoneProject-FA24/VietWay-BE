95d09d0ded4a84afe3f2c79711d66aad1941f266
NeonBlaze101 <dotoan22112003@gmail.com>
2024-10-07 21:55:34 +0700

    add log

--- VietWayManagementAPI/Controllers/BookingPaymentController.cs
+++ VietWayManagementAPI/Controllers/BookingPaymentController.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using VietWay.API.Management.RequestModel;
 using VietWay.Service.DataTransferObject;
 using VietWay.Service.Interface;
@@ -9,15 +11,17 @@ namespace VietWay.API.Management.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BookingPaymentController(IBookingPaymentService bookingPaymentService, IMapper mapper) : ControllerBase
+    public class BookingPaymentController(IBookingPaymentService bookingPaymentService, IMapper mapper, ILogger<BookingPaymentController> logger) : ControllerBase
     {
         private readonly IBookingPaymentService _bookingPaymentService = bookingPaymentService;
         private readonly IMapper _mapper = mapper;
+        private readonly ILogger<BookingPaymentController> _logger = logger;
         [HttpGet("VnPayIPN")]
         public async Task<IActionResult> HandleVnPayIPN(VnPayIPNRequest request)
         {
             VnPayIPN vnPayIPN = _mapper.Map<VnPayIPN>(request);
             await _bookingPaymentService.HandleVnPayIPN(vnPayIPN);
+            _logger.LogInformation("HandleVnPayIPN: {0}", JsonSerializer.Serialize(vnPayIPN));
             return Ok(new
             {
                 RspCode = "00",
