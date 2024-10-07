a914a6c6fa71cb222e3cd3fb4273856f6ff87e60
NeonBlaze101 <dotoan22112003@gmail.com>
2024-10-07 20:07:13 +0700

    Add middleware

---
+++ VietWay.Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace VietWay.Middleware
+{
+    public class ErrorHandlingMiddleware(RequestDelegate next)
+    {
+        private readonly RequestDelegate _next = next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleServerExceptionAsync(context, ex);
+            }
+        }
+        public static Task HandleServerExceptionAsync(HttpContext context, Exception ex)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = JsonSerializer.Serialize(new
+            {
+                error = ex.Message.Split(["\r\n", "\n"], StringSplitOptions.RemoveEmptyEntries),
+                inner = ex.InnerException?.Message.Split(["\r\n", "\n"], StringSplitOptions.RemoveEmptyEntries),
+                detail = ex.StackTrace?.Split(["\r\n","\n"], StringSplitOptions.RemoveEmptyEntries)
+            });
+            context.Response.ContentType = "application/json";
+            var header = new KeyValuePair<string, StringValues>("Access-Control-Allow-Origin", "*");
+            context.Response.Headers.Add(header);
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
