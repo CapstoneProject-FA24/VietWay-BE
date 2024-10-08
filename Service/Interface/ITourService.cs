4e2361741f41e9d7037ed569be6d4fef1ca23563
NeonBlaze101 <dotoan22112003@gmail.com>
2024-10-08 22:03:25 +0700

    update GetTourByTourTemplateId API

--- Service/Interface/ITourService.cs
+++ Service/Interface/ITourService.cs
@@ -9,9 +9,7 @@ namespace VietWay.Service.Interface
         public Task<(int totalCount, List<Tour> items)> GetAllTour(int pageSize, int pageIndex);
         public Task<Tour?> GetTourById(string id);
         public Task<(int totalCount, List<Tour> items)> GetAllScheduledTour(int pageSize, int pageIndex);
-        public Task<(int totalCount, List<Tour> items)> GetAllToursByTemplateIdsAsync(
-            string tourTemplateIds,
-            int pageSize,
-            int pageIndex);
+        public Task<List<Tour>> GetAllToursByTemplateIdsAsync(
+            string tourTemplateIds);
     }
 }
