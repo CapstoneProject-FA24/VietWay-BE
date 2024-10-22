b1170c3ecfbe7e2cb0944f1dbd4e781a7cc53421
NeonBlaze101 <dotoan22112003@gmail.com>
2024-10-23 00:43:54 +0700

    fix error

---
+++ Repository/Migrations/20241022174328_4.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VietWay.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class _4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TourTemplate_Province_ProvinceId",
+                table: "TourTemplate");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TourTemplate_ProvinceId",
+                table: "TourTemplate");
+
+            migrationBuilder.DropColumn(
+                name: "ProvinceId",
+                table: "TourTemplate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProvinceId",
+                table: "TourTemplate",
+                type: "nvarchar(20)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TourTemplate_ProvinceId",
+                table: "TourTemplate",
+                column: "ProvinceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TourTemplate_Province_ProvinceId",
+                table: "TourTemplate",
+                column: "ProvinceId",
+                principalTable: "Province",
+                principalColumn: "ProvinceId");
+        }
+    }
+}
--- VietWay.API.Customer/Controllers/EventController.cs
+++ VietWay.API.Customer/Controllers/EventController.cs
@@ -1,11 +1,31 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using VietWay.API.Customer.ResponseModel;
+using VietWay.Service.DataTransferObject;
 
 namespace VietWay.API.Customer.Controllers
 {
+    /// <summary>
+    /// Event API endpoints
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class EventController : ControllerBase
     {
+
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType<DefaultResponseModel<List<EventPreviewDTO>>>(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllActiveEvent(string? nameSearch, [FromQuery]List<string>? provinceId,
+            [FromQuery]List<string>? eventCategoryIds, DateTime? startDateFrom, DateTime? startDateTo,
+            int? pageSize, int? pageIndex)
+        {
+            return Ok(new DefaultResponseModel<List<EventPreviewDTO>>()
+            {
+                Message = "Success",
+
+                StatusCode = StatusCodes.Status200OK
+            });
+        }
     }
 }
