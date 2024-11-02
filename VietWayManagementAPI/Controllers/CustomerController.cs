8c34c944647f6c550d4fb059106844bb09496279
NeonBlaze101 <dotoan22112003@gmail.com>
2024-11-02 10:02:39 +0700

    add customer delete/umdelete api

---
+++ VietWayManagementAPI/Controllers/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VietWay.Service.Management.Interface;
+using VietWay.Util.TokenUtil;
+
+namespace VietWay.API.Management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController(ICustomerService customerService, ITokenHelper tokenHelper) : ControllerBase
+    {
+        private readonly ICustomerService _customerService = customerService;
+        private readonly ITokenHelper _tokenHelper = tokenHelper;
+        [HttpPatch("{customerId}")]
+        public async Task<IActionResult> ChangeCustomerStatus(string customerId, bool isDeleted)
+        {
+            string? managerId = _tokenHelper.GetAccountIdFromToken(HttpContext);
+            if (managerId == null)
+            {
+                return Unauthorized();
+            }
+            await _customerService.ChangeCustomerStatus(customerId, managerId, isDeleted);
+            return Ok();
+        }
+    }
+}
