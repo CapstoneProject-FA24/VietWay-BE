023666f41c798de08564d11aa7a86acd8b68559b
NeonBlaze101 <dotoan22112003@gmail.com>
2024-11-03 23:03:48 +0700

    fix

--- VietWayManagementAPI/Controllers/PostController.cs
+++ VietWayManagementAPI/Controllers/PostController.cs
@@ -53,6 +53,35 @@ namespace VietWay.API.Management.Controllers
                 StatusCode = StatusCodes.Status200OK
             });
         }
+        /// <summary>
+        /// ✅🔐[Staff] Get post by ID
+        /// </summary>
+        /// <returns>Post detail</returns>
+        /// <response code="200">Success</response>
+        /// <response code="401">Unauthorized</response>
+        /// <response code="404">Not found</response>
+        [HttpGet("{postId}")]
+        [Produces("application/json")]
+        [ProducesResponseType<DefaultResponseModel<PostPreviewDTO>>(StatusCodes.Status200OK)]
+        [ProducesResponseType<DefaultResponseModel<object>>(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetPostById(string postId)
+        {
+            PostPreviewDTO? post = await _postService.GetPostByIdAsync(postId);
+            if (null == post)
+            {
+                return NotFound(new DefaultResponseModel<object>
+                {
+                    Message = "Not found",
+                    StatusCode = StatusCodes.Status404NotFound
+                });
+            }
+            return Ok(new DefaultResponseModel<PostPreviewDTO>
+            {
+                Message = "Get post successfully",
+                StatusCode = StatusCodes.Status200OK,
+                Data = post
+            });
+        }
 
         /// <summary>
         /// ✅🔐[Staff] Create new post
