3a6daad225453ca7a767658963d63a0587675b1e
NeonBlaze101 <dotoan22112003@gmail.com>
2024-11-15 16:33:13 +0700

    add migration

---
+++ Repository/Migrations/20241115093245_1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VietWay.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class _1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "TourReview",
+                newName: "Review");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Review",
+                table: "TourReview",
+                newName: "Content");
+        }
+    }
+}
