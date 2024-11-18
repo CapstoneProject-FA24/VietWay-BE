ea4365a968eae43d2e265c197e9462084566f9fe
NeonBlaze101 <dotoan22112003@gmail.com>
2024-11-18 17:44:01 +0700

    add system prompt to gemini

--- VietWay.Service.ThirdParty/GoogleGemini/GeminiClient.cs
+++ VietWay.Service.ThirdParty/GoogleGemini/GeminiClient.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Json;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -9,40 +10,52 @@ using System.Threading.Tasks;
 
 namespace VietWay.Service.ThirdParty.GoogleGemini
 {
-    public class GeminiClient(string apiKey, string apiEndpoint)
+    public class GeminiClient(string apiKey, string apiEndpoint, string? systemPrompt = null)
     {
         private readonly string _apiKey = apiKey;
         private readonly string _apiEndpoint = apiEndpoint;
+        private readonly string? _systemPrompt = systemPrompt;
+
         public async Task<string> QueryAsync(string content)
         {
             using HttpClient httpClient = new();
-            HttpResponseMessage response = await httpClient.PostAsJsonAsync($"{_apiEndpoint}:generateContent?key={_apiKey}", new
+            GeminiChatRequest chatRequest = new()
             {
-                Contents = new List<Content>
-                {
+                Contents =
+                [
                     new()
-                    {
-                        Parts =
-                        [
-                            new() { Text = content }
-                        ],
-                        Role = "user"
-                    }
-                },
-            });
+                        {
+                            Parts =
+                            [
+                                new() { Text = content }
+                            ],
+                            Role = "user"
+                        }
+                ],
+            };
+            if (_systemPrompt != null)
+            {
+                chatRequest.SystemInstruction = new SystemInstruction { Parts = new Part() { Text = _systemPrompt } };
+            }
+            HttpResponseMessage response = await httpClient.PostAsJsonAsync($"{_apiEndpoint}:generateContent?key={_apiKey}", chatRequest);
             response.EnsureSuccessStatusCode();
             JsonDocument jsonDocument = await JsonDocument.ParseAsync(await response.Content.ReadAsStreamAsync());
             JsonElement root = jsonDocument.RootElement;
             return root.GetProperty("candidates")[0].GetProperty("content").GetProperty("parts")[0].GetProperty("text").GetString();
-
         }
+
         public async Task<string> ChatAsync(List<Content> contents)
         {
             using HttpClient httpClient = new();
-            HttpResponseMessage response = await httpClient.PostAsJsonAsync($"{_apiEndpoint}:generateContent?key={_apiKey}", new
+            GeminiChatRequest chatRequest = new()
             {
                 Contents = contents
-            });
+            };
+            if (_systemPrompt != null)
+            {
+                chatRequest.SystemInstruction = new SystemInstruction { Parts = new Part() { Text = _systemPrompt } };
+            }
+            HttpResponseMessage response = await httpClient.PostAsJsonAsync($"{_apiEndpoint}:generateContent?key={_apiKey}",chatRequest);
             response.EnsureSuccessStatusCode();
             JsonDocument jsonDocument = await JsonDocument.ParseAsync(await response.Content.ReadAsStreamAsync());
             JsonElement root = jsonDocument.RootElement;
--- VietWay.Service.ThirdParty/GoogleGemini/GeminiService.cs
+++ VietWay.Service.ThirdParty/GoogleGemini/GeminiService.cs
@@ -15,7 +15,9 @@ namespace VietWay.Service.ThirdParty.GoogleGemini
                 throw new Exception("GEMINI_AI_API_KEY is not set in environment variables");
             string apiEndpoint = Environment.GetEnvironmentVariable("GEMINI_AI_API_ENDPOINT") ??
                 throw new Exception("GEMINI_AI_API_ENDPOINT is not set in environment variables");
-            _geminiClient = new GeminiClient(apiKey, apiEndpoint);
+            string? systemPrompt = Environment.GetEnvironmentVariable("GEMINI_AI_SYSTEM_PROMPT");
+
+            _geminiClient = new GeminiClient(apiKey, apiEndpoint, systemPrompt);
         }
 
         public async Task<string> ChatAsync(List<Content> contents)
