b2d070447cc1a2045f50e68296ad601ab45ed76c
KaiserSlenderbrine <slenderbrine17@gmail.com>
2024-11-24 21:12:38 +0700

    update-create-staff-account

---
+++ VietWayManagementAPI/RequestModel/CreateStaffAccountRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VietWay.API.Management.RequestModel
+{
+    public class CreateStaffAccountRequest
+    {
+        public string? Email { get; set; }
+        [RegularExpression(@"^0\d{9}$")]
+        public required string PhoneNumber { get; set; }
+        [Required]
+        public required string FullName { get; set; }
+    }
+}
