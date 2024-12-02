d44b65139a38dd65431a630b356816f6ce8cbe8f
NeonBlaze101 <dotoan22112003@gmail.com>
2024-12-02 15:46:15 +0700

    fix

--- VietWay.Service.Customer/Implementation/BookingService.cs
+++ VietWay.Service.Customer/Implementation/BookingService.cs
@@ -112,7 +112,7 @@ namespace VietWay.Service.Customer.Implementation
 
                 decimal refundPercentCost = booking.Tour.TourRefundPolicies?
                     .Where(x => x.CancelBefore > _timeZoneHelper.GetUTC7Now())
-                    .OrderBy(x => x.RefundPercent)
+                    .OrderBy(x => x.CancelBefore)
                     .Select(x => x.RefundPercent)
                     .FirstOrDefault() ?? 100;
 
