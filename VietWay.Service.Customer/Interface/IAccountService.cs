c2a90a1003234d40d22ff050ebd634eaf2fe1afa
H03 <hieupnse171321@fpt.edu.vn>
2024-12-05 00:29:29 +0700

    update login get fullname

--- VietWay.Service.Customer/Interface/IAccountService.cs
+++ VietWay.Service.Customer/Interface/IAccountService.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VietWay.Repository.EntityModel;
+using VietWay.Service.Customer.DataTransferObject;
 
 namespace VietWay.Service.Customer.Interface
 {
     public interface IAccountService
     {
         public Task<string> ConfirmResetPasswordOtpAsync(string phoneNumber, string otp);
-        public Task<Account?> LoginAsync(string emailOrPhone, string password);
-        public Task<Account?> LoginWithGoogleAsync(string idToken);
+        public Task<CredentialDTO?> LoginAsync(string emailOrPhone, string password);
+        public Task<CredentialDTO?> LoginWithGoogleAsync(string idToken);
         public Task ResetPasswordAsync(string accountId, string phoneNumber, string newPassword);
         public Task SendResetPasswordOtpAsync(string phoneNumber);
     }
