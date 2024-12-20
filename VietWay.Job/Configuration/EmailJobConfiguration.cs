b116df0bbe13e80700ad0aa951e2f67172adb7ee
NeonBlaze101 <dotoan22112003@gmail.com>
2024-12-20 11:02:05 +0700

    send email warning

--- VietWay.Job/Configuration/EmailJobConfiguration.cs
+++ VietWay.Job/Configuration/EmailJobConfiguration.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace VietWay.Job.Configuration
 {
     public class EmailJobConfiguration(string cancelBookingTemplate, string confirmBookingTemplate, string vietwayCancelBookingTemplate, 
-        string resetPasswordTemplate, string bookingExpiredTemplate, string bookingChangedTemplate)
+        string resetPasswordTemplate, string bookingExpiredTemplate, string bookingChangedTemplate, string warningClosedTourNotEnoughParticipant)
     {
         private string _cancelBookingTemplate = cancelBookingTemplate;
         private string _confirmBookingTemplate = confirmBookingTemplate;
@@ -15,11 +15,13 @@ namespace VietWay.Job.Configuration
         private string _resetPasswordTemplate = resetPasswordTemplate;
         private string _bookingExpiredTemplate = bookingExpiredTemplate;
         private string _bookingChangedTemplate = bookingChangedTemplate;
+        private string _warningClosedTourNotEnoughParticipant = warningClosedTourNotEnoughParticipant;
         public string ConfirmBookingTemplate { get => _confirmBookingTemplate; }
         public string CancelBookingTemplate { get => _cancelBookingTemplate; }
         public string VietwayCancelBookingTemplate { get => _vietwayCancelBookingTemplate; }
         public string ResetPasswordTemplate { get => _resetPasswordTemplate; }
         public string BookingExpiredTemplate { get => _bookingExpiredTemplate; }
         public string BookingChangedTemplate { get => _bookingChangedTemplate; }
+        public string WarningClosedTourNotEnoughParticipantTemplate { get => _warningClosedTourNotEnoughParticipant; }
     }
 }
