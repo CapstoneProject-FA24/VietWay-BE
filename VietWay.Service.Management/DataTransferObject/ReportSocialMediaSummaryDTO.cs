944538a84cf915b1cc84955f07954b672e1c1bc6
NeonBlaze101 <dotoan22112003@gmail.com>
2024-12-30 21:18:44 +0700

    add report dtos

---
+++ VietWay.Service.Management/DataTransferObject/ReportSocialMediaSummaryDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VietWay.Service.Management.DataTransferObject
+{
+    public class ReportSocialMediaSummaryDTO
+    {
+        public List<string>? Dates { get; set; }
+        public List<int>? FacebookReferrals { get; set; }
+        public List<int>? FacebookComments { get; set; }
+        public List<int>? FacebookShares { get; set; }
+        public List<int>? FacebookReactions { get; set; }
+        public List<int>? FacebookImpressions { get; set; }
+        public List<int>? XRetweets { get; set; }
+        public List<int>? XReplies { get; set; }
+        public List<int>? XLikes { get; set; }
+        public List<int>? XImpressions { get; set; }
+        public List<int>? XReferrals { get; set; }
+    }
+}
